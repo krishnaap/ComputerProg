b4bb21d43dee84e24f33442377a523794f7a43a1
Krishnakumar AP <krishnaap7@gmail.com>
2024-09-29 14:10:58 -0500

    Add files via upload

---
+++ Lesson 1 Assignment.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"Hello world"
+msg="Hello world"
+msg
+msg2=hi
+12=13
+
+12+13
+25-12
+12*13
+256/64
+12==13
+12==12.0
+
+name ="Krishnakumar AP"
+
+print(f"Completed by",name)
